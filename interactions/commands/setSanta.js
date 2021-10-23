4272908854d1a2a306683723f4f64d0f93598875
JamesSigler <james64.sigler@ttu.edu>
2021-10-23 14:47:37 -0600

    SS Create List

---
+++ interactions/commands/setSanta.js
@@ -0,0 +1,69 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('set-santa')
+		.setDescription('Tell recievers who they are giving a gift to.'),
+	async execute(interaction) {
+        const reciever = await client.prisma.sSReciever.findMany({})
+		const senders = await client.prisma.sSSender.findMany({})
+
+
+        console.log(attachUser(reciever, senders))
+		await interaction.reply({content:"logged to console", ephemeral:true})
+	},   
+
+    isGuild : true
+};
+
+async function attachUser (recievers, senders) {
+	val = []
+	added=[]
+	output=[]
+
+    for (var key in recievers){
+		val.push(recievers[key].ID)
+	}
+
+	shuffle(val)
+
+	for(var reciever in recievers){
+		for(var sender in val){
+			if (recievers[reciever].ID == val[sender]){
+				console.log("Skipped")
+				continue
+			}
+	
+			for(var obj in senders){
+				if(senders[obj].ID==val[sender]){
+					if((senders[obj].HasINTER == 1 && recievers[reciever].RequiresINTER == 1)  || (senders[obj].HasEU == 1 && recievers[reciever].RequiresEU == 1)){
+						
+						if(added.includes(recievers[reciever].ID))
+						{
+							continue
+						}
+						added.push(recievers[reciever].ID)
+						output.push(recievers[reciever].ID + " " + senders[obj].ID)
+						val.splice(sender, 1);
+					}
+				}
+			}	 
+		}
+	}	
+	console.log(output)
+}
+
+
+async function shuffle(array){
+	let currentIndex = array.length, randomIndex 
+
+	while (currentIndex != 0){
+		randomIndex = Math.floor(Math.random()*currentIndex)
+		currentIndex--
+    	[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+	}
+  	return array
+}
+
