c7fb5b142e005ab32b77f8a07319a0984d35e097
Niels Larmuseau <niels@larmuseau.org>
2021-10-27 14:41:53 +0200

    keep track of joins and leaving

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,19 @@
+const { MessageEmbed,MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+    name: 'guildMemberAdd',
+    execute(GuildMember) {
+
+        let embed = new MessageEmbed()
+            .setColor('#FD8612')
+            .setTitle(  `${GuildMember.user.username} joined`)
+            .setAuthor(`${GuildMember.user.username}`, 'https://cdn.discordapp.com/avatars/'+GuildMember.user.id+'/'+GuildMember.user.avatar)
+            .addFields(
+                { name: 'ID', value: `${GuildMember.id}`,inline: false  },
+                { name: 'name + discriminator', value: `${GuildMember.user.username}#${GuildMember.user.discriminator}`,inline: false  },
+                { name: 'account created', value: `${GuildMember.user.createdAt}`,inline: false  },
+            )
+            .setTimestamp()
+        client.logChannel.send({embeds: [embed]})
+    },
+};
