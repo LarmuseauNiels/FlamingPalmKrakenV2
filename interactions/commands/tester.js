9061a0edee3c5049e8be199e1cf43d39c3062575
Niels Larmuseau <niels@larmuseau.org>
2022-01-06 21:46:03 +0100

    reset button

--- interactions/commands/tester.js
+++ interactions/commands/tester.js
@@ -7,12 +7,20 @@ module.exports = {
         .setName('tester')
         .setDescription('testingcommand'),
     async execute(interaction) {
+		await client.prisma.channel.findMany({select: {
+				ID: true
+			}}).then(channel =>trackedChannels = channel);
 
 		client.guilds.fetch('530537522355240961').then(guild =>
 		guild.channels.fetch().then(channels => {
 			let text = "";
 			channels.forEach(channel => {
-				if (channel.isVoice()) text = text + channel.name + " ";
+				if (channel.isVoice()) {
+					text = text + channel.name ;
+					if (trackedChannels.includes(channel.id)) text = text + "%";
+					text = text + "  ";
+				}
+
 			})
 			interaction.reply({content: text, ephemeral: false});
 		})
