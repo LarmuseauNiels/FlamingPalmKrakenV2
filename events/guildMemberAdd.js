5298147f717547cab2a71cf0d63e18c99d4bc722
Niels Larmuseau <niels@larmuseau.org>
2022-03-09 22:58:59 +0100

    ss

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,32 +3,26 @@ const { MessageEmbed,MessageActionRow, MessageButton } = require('discord.js');
 module.exports = {
     name: 'guildMemberAdd',
     execute(GuildMember) {
-
-
-        let embed = new MessageEmbed()
-            .setColor('#FD8612')
-            .setTitle(  `${GuildMember.user.username} joined`)
-            .setAuthor(`${GuildMember.user.username}`, 'https://cdn.discordapp.com/avatars/'+GuildMember.user.id+'/'+GuildMember.user.avatar)
-            .addFields(
-                { name: 'ID ', value: `${GuildMember.id}`,inline: false  },
-                { name: 'name & discriminator', value: `${GuildMember.user.username}#${GuildMember.user.discriminator}`,inline: false  },
-                { name: 'account created', value: `${GuildMember.user.createdAt}`,inline: false  },
-            )
-            .setTimestamp()
-        client.logChannel.send({embeds: [embed]})
         // just a test
         const cachedInvites = client.invites.get(GuildMember.guild.id);
-        client.logChannel.send(JSON.stringify(cachedInvites) );
-
+        const oldinvites = cachedInvites.map(i => {return {code: i.code, uses: i.uses}});
+        console.log(oldinvites);
         GuildMember.guild.invites.fetch().then(newInvites => {
             console.log("got invites")
-            client.invites.set(GuildMember.guild.id, newInvites);
-            const usedInvite = newInvites.find(invite => cachedInvites.get(invite.code).uses < invite.uses);
-            console.log(client.invites);
-            console.log(cachedInvites);
-            console.log(newInvites);
-            console.log(usedInvite);
-            client.logChannel.send(JSON.stringify(usedInvite) + "\r\n" + JSON.stringify(newInvites));
+            const usedInvite = newInvites.find(invite => oldinvites.find(i => i.code == invite.code).uses < invite.uses);
+            let embed = new MessageEmbed()
+                .setColor('#FD8612')
+                .setTitle(  `${GuildMember.user.username} joined`)
+                .setAuthor(`${GuildMember.user.username}`, 'https://cdn.discordapp.com/avatars/'+GuildMember.user.id+'/'+GuildMember.user.avatar)
+                .addFields(
+                    { name: 'ID ', value: `${GuildMember.id}`,inline: false  },
+                    { name: 'name & discriminator', value: `${GuildMember.user.username}#${GuildMember.user.discriminator}`,inline: false  },
+                    { name: 'account created', value: `${GuildMember.user.createdAt}`,inline: false  },
+                    { name: 'link code', value: `${usedInvite.code}`,inline: false  },
+                    { name: 'inviter', value: `${usedInvite.inviter.username}`,inline: false  },
+                )
+                .setTimestamp()
+            client.logChannel.send({embeds: [embed]})
         });
     },
 };
