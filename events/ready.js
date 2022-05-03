e0f83d51dd8108e88ffa631a1d25851aaf731ab8
Niels Larmuseau <nielslarmuseau@gmail.com>
2022-05-03 18:34:26 +0200

    no more permissions

--- events/ready.js
+++ events/ready.js
@@ -12,12 +12,12 @@ module.exports = {
 		);
 		require("../modules/statistics.js")(client);
 		require("../modules/webapi.js")(client);
-		client.guilds.cache.get("530537522355240961")?.commands.fetch().then(commands => commands.forEach(command =>{
-			let permissions = client.commands.get(command.name)?.permissions;
-			if (permissions != null){
-				command.permissions.set( { permissions });
-			}
-		}));
+		// client.guilds.cache.get("530537522355240961")?.commands.fetch().then(commands => commands.forEach(command =>{
+		// 	let permissions = client.commands.get(command.name)?.permissions;
+		// 	if (permissions != null){
+		// 		command.permissions.set( { permissions });
+		// 	}
+		// }));
 		// Collection is an enhanced Map which we are going to save our invites to.
 		const guildInvites = new Collection();
 		client.invites = guildInvites;
