2e90560811f7fe81ad852930927c66ba9f00da0c
Niels Larmuseau <niels@larmuseau.org>
2022-09-20 17:49:26 +0200

    islander

--- interactions/commands/island.js
+++ interactions/commands/island.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder, AttachmentBuilder } = require('@discordjs/builders');
-const { EmbedBuilder,ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { EmbedBuilder,AttachmentBuilder,SlashCommandBuilder } = require('discord.js');
 const Canvas = require("@napi-rs/canvas");
 
 module.exports = {
@@ -12,13 +11,13 @@ module.exports = {
 		// 	let island = member.i_Island;
             const embed = new EmbedBuilder()
             .setColor('#FD8612')
-            .setTitle(  "Island")
-            .setAuthor(member.DisplayName, 'https://cdn.discordapp.com/avatars/'+member.ID+'/'+interaction.user.avatar, 'https://flamingpalm.com')
-            //.setDescription('Some description here')
-            //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-
-            .setTimestamp()
-            .setFooter('Work in progress Islander game', 'https://flamingpalm.com/images/FlamingPalmLogoSmall.png');
+				.setTitle(  "Island")
+            // .setAuthor(member.DisplayName, 'https://cdn.discordapp.com/avatars/'+member.ID+'/'+interaction.user.avatar, 'https://flamingpalm.com')
+            // //.setDescription('Some description here')
+            // //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+			//
+            // .setTimestamp()
+            // .setFooter('Work in progress Islander game', 'https://flamingpalm.com/images/FlamingPalmLogoSmall.png');
 			// island.i_Building_Island.forEach(ibi =>{
 			// 	let bl = ibi.i_BuildingLevel;
 			//  	embed.addField(bl.Name, 'lvl ' + bl.Level, true);
@@ -26,12 +25,12 @@ module.exports = {
 		const canvas = Canvas.createCanvas(950, 650);
 		const ctx = canvas.getContext('2d');
 
-		const island = await Canvas.loadImage('sprites/starting.png');
-		const header = await Canvas.loadImage('sprites/buttonLong_brown.png');
-		const foodIcon = await Canvas.loadImage('sprites/food.png');
-		const dudeIcon = await Canvas.loadImage('sprites/dude.png');
-		const stoneIcon = await Canvas.loadImage('sprites/stone.png');
-		const woodIcon = await Canvas.loadImage('sprites/wood.png');
+		const island = await Canvas.loadImage('../sprites/starting.png');
+		const header = await Canvas.loadImage('../sprites/buttonLong_brown.png');
+		const foodIcon = await Canvas.loadImage('../sprites/food.png');
+		const dudeIcon = await Canvas.loadImage('../sprites/dude.png');
+		const stoneIcon = await Canvas.loadImage('../sprites/stone.png');
+		const woodIcon = await Canvas.loadImage('../sprites/wood.png');
 
 		ctx.drawImage(island, -4, -180, 1000, 1000);
 		ctx.drawImage(header, 80, 10, 190, 49);
