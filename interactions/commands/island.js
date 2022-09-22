852bb9cc1367c474d03fe743a5f2d9e8d0b0cb28
Niels Larmuseau <niels@larmuseau.org>
2022-09-22 20:30:21 +0200

    islander

--- interactions/commands/island.js
+++ interactions/commands/island.js
@@ -11,27 +11,16 @@ module.exports = {
             const embed = new EmbedBuilder()
             .setColor('#FD8612')
 				.setTitle(  "Island")
-            // .setAuthor(member.DisplayName, 'https://cdn.discordapp.com/avatars/'+member.ID+'/'+interaction.user.avatar, 'https://flamingpalm.com')
-            // //.setDescription('Some description here')
-            // //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-			//
-            // .setTimestamp()
-            // .setFooter('Work in progress Islander game', 'https://flamingpalm.com/images/FlamingPalmLogoSmall.png');
-			// island.i_Building_Island.forEach(ibi =>{
-			// 	let bl = ibi.i_BuildingLevel;
-			//  	embed.addField(bl.Name, 'lvl ' + bl.Level, true);
-			// })
-
 			client.islander.GetImage(interaction.user.id,island).then(image => {
 				interaction.reply({ files: [image]  , ephemeral: false });
 			})
 
 
             //interaction.followUp({ embeds: [hiddenEmbed] , components: [row] ,ephemeral: true });
-         },error =>{
-		 	 console.log(error);
-              interaction.reply({ content: "Island not available", ephemeral: true });
-         });
+		 }).catch(err => {
+			console.log(err);
+			interaction.reply({ content: 'You do not have an island yet.', ephemeral: true });
+		 });
 	},
     isGuild: true,
 };
