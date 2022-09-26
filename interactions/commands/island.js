43bb3dbd4677b83d213405d8800544d34d0ba789
Niels Larmuseau <niels@larmuseau.org>
2022-09-26 21:26:10 +0200

    buttons

--- interactions/commands/island.js
+++ interactions/commands/island.js
@@ -42,8 +42,10 @@ module.exports = {
 				);
 
 			client.islander.GetImage(interaction.user.id,island).then(image => {
-				interaction.reply({ content: text, files: [image] , ephemeral: false });
-				interaction.followUp({ content: 'menu', components: [row] ,ephemeral: true });
+				interaction.reply({ content: text, files: [image] , ephemeral: false }).then(() => {
+					interaction.followUp({ content: 'menu', components: [row] ,ephemeral: true });
+				});
+				//
 			})
 
             //interaction.followUp({ embeds: [hiddenEmbed] , components: [row] ,ephemeral: true });
