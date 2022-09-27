7df19b9070d49b12795f9622b3dc884c7c6285a6
Niels Larmuseau <niels@larmuseau.org>
2022-09-27 14:14:29 +0200

    emojis

--- interactions/buttons/gather.js
+++ interactions/buttons/gather.js
@@ -8,15 +8,18 @@ module.exports = {
                 new ButtonBuilder()
                     .setCustomId('gatherWood')
                     .setLabel('Wood')
-                    .setStyle(ButtonStyle.Secondary),
+                    .setStyle(ButtonStyle.Secondary)
+                    .setEmoji('1024292209559879742'),
                 new ButtonBuilder()
                     .setCustomId('gatherFood')
                     .setLabel('Food')
-                    .setStyle(ButtonStyle.Secondary),
+                    .setStyle(ButtonStyle.Secondary)
+                    .setEmoji('1024292206409953291'),
                 new ButtonBuilder()
                     .setCustomId('gatherStone')
                     .setLabel('Stone')
                     .setStyle(ButtonStyle.Secondary)
+                    .setEmoji('1024292208414822450')
             );
             interaction.reply({ components: [row] , ephemeral: true });
     }
