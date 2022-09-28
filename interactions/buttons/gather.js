417fa1d0059e17ddeb1001e85d621762c13d2c73
Niels Larmuseau <niels@larmuseau.org>
2022-09-28 08:26:33 +0200

    only icon test

--- interactions/buttons/gather.js
+++ interactions/buttons/gather.js
@@ -7,19 +7,19 @@ module.exports = {
             .addComponents(
                 new ButtonBuilder()
                     .setCustomId('gatherWood')
-                    .setLabel('Wood')
+                    //.setLabel('Wood')
                     .setStyle(ButtonStyle.Secondary)
-                    .setEmoji('1024292209559879742'),
+                    .setEmoji('1024566960840769576'),
                 new ButtonBuilder()
                     .setCustomId('gatherFood')
-                    .setLabel('Food')
+                    //.setLabel('Food')
                     .setStyle(ButtonStyle.Secondary)
-                    .setEmoji('1024292206409953291'),
+                    .setEmoji('1024566956130582628'),
                 new ButtonBuilder()
                     .setCustomId('gatherStone')
-                    .setLabel('Stone')
+                    //.setLabel('Stone')
                     .setStyle(ButtonStyle.Secondary)
-                    .setEmoji('1024292208414822450')
+                    .setEmoji('1024566959456649256')
             );
             interaction.reply({ components: [row] , ephemeral: true });
     }
