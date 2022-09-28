d4a94ae549dd0074085d6a5d767202ec4dba2f72
Niels Larmuseau <niels@larmuseau.org>
2022-09-28 21:47:51 +0200

    typescript compatible

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 
-if (fs.existsSync("./privateconfig.json")) {
-  this.privateconfig = require("./privateconfig.json");
-  module.exports = {
-    clientId: "534686392589221898", //"294470823627063296",
-    token: this.privateconfig.token,
-    DBHOST: this.privateconfig.DBHOST,
-    DBPASS: this.privateconfig.DBPASS,
-  };
+if (false) {
+  /*
+    this.privateconfig = require("./privateconfig.json");
+    module.exports = {
+      clientId: "534686392589221898", //"294470823627063296",
+      token: this.privateconfig.token,
+      DBHOST: this.privateconfig.DBHOST,
+      DBPASS: this.privateconfig.DBPASS,
+    };
+    */
 } else {
   module.exports = {
     clientId: "534686392589221898",
