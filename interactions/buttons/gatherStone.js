1a218eff700b018247777e17e3f202cfa52020b4
Niels Larmuseau <niels@larmuseau.org>
2022-09-30 23:33:24 +0200

    gathering button fix

--- interactions/buttons/gatherStone.js
+++ interactions/buttons/gatherStone.js
@@ -3,7 +3,7 @@ const {} = require("discord.js");
 module.exports = {
   name: "gatherStone",
   async execute(interaction) {
-    if (interaction.user.id !== interaction.message.author.id) {
+    if (interaction.user.id !== interaction.message.interaction.user.id) {
       interaction.reply({
         content: "You can't interact with someone else's buttons!",
         ephemeral: true,
