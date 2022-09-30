fb3cdf61a3d4f2e92af8701a35839692bfc8e01f
Niels Larmuseau <niels@larmuseau.org>
2022-10-01 00:10:22 +0200

    js test

--- interactions/buttons/build.js
+++ interactions/buttons/build.js
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
   name: "build",
