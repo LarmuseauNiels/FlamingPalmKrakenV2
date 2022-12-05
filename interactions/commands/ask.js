2c3c6e2a5121715f604cfb1fdb918306dc4af240
Niels Larmuseau <niels@larmuseau.org>
2022-12-05 17:05:36 +0100

    GPT 3 chat

---
+++ interactions/commands/ask.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+//const { EmbedBuilder,ActionRowBuilder, ButtonBuilder } = require('discord.js');
+
+module.exports = {
+  name: "chat",
+  data: new SlashCommandBuilder()
+    .setName("chat")
+    .setDescription("chat with the kraken")
+    .addStringOption((option) =>
+      option
+        .setName("message")
+        .setDescription("The message to send")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const message = interaction.options.getString("message");
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message}`,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"],
+    });
+
+    interaction.reply(response);
+  },
+  isGuild: true,
+};
