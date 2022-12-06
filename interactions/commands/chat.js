ccd6bc73f3416c5fc0148c0b22b3be9783bc7e21
Niels Larmuseau <niels@larmuseau.org>
2022-12-06 10:28:51 +0100

    add answer bracket

--- interactions/commands/chat.js
+++ interactions/commands/chat.js
@@ -24,7 +24,7 @@ module.exports = {
       await interaction.deferReply({ ephemeral: true });
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \n`,
+        prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \nA:`,
         temperature: 0,
         max_tokens: 100,
         top_p: 1,
