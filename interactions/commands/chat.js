dabb44e8782273ac1f896cc24e0e3bb428e2aea8
Niels Larmuseau <niels@larmuseau.org>
2022-12-06 08:22:05 +0100

    defer reply

--- interactions/commands/chat.js
+++ interactions/commands/chat.js
@@ -20,6 +20,7 @@ module.exports = {
     });
     const message = interaction.options.getString("message");
     const openai = new OpenAIApi(configuration);
+    await interaction.deferReply();
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \n`,
@@ -30,7 +31,7 @@ module.exports = {
       presence_penalty: 0.0,
       stop: ["\n"],
     });
-    interaction.reply(response);
+    await interaction.reply(response);
   },
   isGuild: true,
 };
