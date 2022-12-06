5886bc5aa65af48118da03535aaeda3b3bd0eeca
Niels Larmuseau <niels@larmuseau.org>
2022-12-06 10:39:29 +0100

    not ephemeral

--- interactions/commands/chat.js
+++ interactions/commands/chat.js
@@ -21,10 +21,10 @@ module.exports = {
     const message = interaction.options.getString("message");
     const openai = new OpenAIApi(configuration);
     try {
-      await interaction.deferReply({ ephemeral: true });
+      await interaction.deferReply();
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \nA:`,
+        prompt: `I am the krakan a highly intelligent question answering bot designed for the flaming palm gaming clan. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \nA:`,
         temperature: 0,
         max_tokens: 100,
         top_p: 1,
@@ -38,7 +38,7 @@ module.exports = {
       interaction.editReply(data.choices[0].text || "no response");
     } catch (e) {
       console.log(e);
-      interaction.editReply({ content: "error", ephemeral: true });
+      interaction.editReply({ content: "error" });
     }
   },
   isGuild: true,
