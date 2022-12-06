3b82898b54573648c3e04a8ab085fad78ae09145
Niels Larmuseau <niels@larmuseau.org>
2022-12-06 08:43:33 +0100

    error catching

--- interactions/commands/chat.js
+++ interactions/commands/chat.js
@@ -20,18 +20,28 @@ module.exports = {
     });
     const message = interaction.options.getString("message");
     const openai = new OpenAIApi(configuration);
-    await interaction.deferReply();
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \n`,
-      temperature: 0,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      stop: ["\n"],
-    });
-    await interaction.editReply(response);
+    try {
+      await interaction.deferReply({ ephemeral: true });
+      await openai
+        .createCompletion({
+          model: "text-davinci-003",
+          prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer,  I will answer with a sarcastic response.\n\nQ: ${message} \n`,
+          temperature: 0,
+          max_tokens: 100,
+          top_p: 1,
+          frequency_penalty: 0.0,
+          presence_penalty: 0.0,
+          stop: ["\n"],
+        })
+        .then((response) => {
+          if (response) interaction.editReply(response);
+          else
+            interaction.editReply({ content: "no response", ephemeral: true });
+        });
+    } catch (e) {
+      console.log(e);
+      interaction.editReply({ content: "error", ephemeral: true });
+    }
   },
   isGuild: true,
 };
