cee8f0d4c711e5963fafe567c807c61d68924dde
Niels Larmuseau <niels@larmuseau.org>
2023-01-12 14:40:26 +0100

    menu command also command

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,21 +7,21 @@ module.exports = {
         let button = global.client.buttons.get(buttonId);
         await button.execute(interaction);
       }
-      if (interaction.isCommand()) {
+      if (interaction.isUserContextMenuCommand()) {
         console.log(interaction.commandName);
-        let command = global.client.commands.get(interaction.commandName);
-        await command.execute(interaction);
+        let menu = global.client.contextMenus.get(interaction.commandName);
+        await menu.execute(interaction);
+      } else {
+        if (interaction.isCommand()) {
+          console.log(interaction.commandName);
+          let command = global.client.commands.get(interaction.commandName);
+          await command.execute(interaction);
+        }
       }
       if (interaction.isModalSubmit()) {
         let modalId = interaction.customId.split("_")[0];
         let modal = global.client.modals.get(modalId);
         await modal.execute(interaction);
-      }
-      if (interaction.isUserContextMenuCommand()) {
-        console.log(interaction.commandName);
-        let menu = global.client.contextMenus.get(interaction.commandName);
-        console.log(menu);
-        //await menu.execute(interaction);
       }
     } catch (error) {
       global.client.log(error);
