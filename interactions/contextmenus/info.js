0b3f7e0f0877f2ae5e7d4361d000823bb3e10fab
Niels Larmuseau <niels@larmuseau.org>
2023-01-13 09:38:44 +0100

    see points

--- interactions/contextmenus/info.js
+++ interactions/contextmenus/info.js
@@ -2,14 +2,26 @@ const {
   ContextMenuCommandBuilder,
   ApplicationCommandType,
   EmbedBuilder,
+  PermissionFlagsBits,
 } = require("discord.js");
 module.exports = {
   name: "info",
   data: new ContextMenuCommandBuilder()
     .setName("info")
-    .setType(ApplicationCommandType.User),
+    .setType(ApplicationCommandType.User)
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
+    let dbinfo = await global.client.prisma.members.findUnique({
+      where: { ID: interaction.targetId },
+      include: {
+        Points: true,
+        PointHistory: {
+          orderBy: { TimeStamp: "desc" },
+          take: 1,
+        },
+      },
+    });
     interaction.guild.members.fetch(interaction.targetId).then((member) => {
       let embed = new EmbedBuilder()
         .setTitle(member.user.username)
@@ -19,9 +31,28 @@ module.exports = {
         })
         .addFields(
           { name: "ID", value: `${member.id}`, inline: false },
-          { name: "Joined", value: `${member.joinedAt}`, inline: false },
-          { name: "Created", value: `${member.user.createdAt}`, inline: false },
-          { name: "Roles", value: `${member.roles.cache.size}`, inline: false }
+          {
+            name: "Joined",
+            value: `${member.joinedAt.toISOString()}`,
+            inline: false,
+          },
+          {
+            name: "Created",
+            value: `${member.user.createdAt.toISOString()}`,
+            inline: false,
+          },
+          {
+            name: "Points",
+            value: `${member.Points.TotalPoints} :palm_tree:`,
+            inline: false,
+          },
+          {
+            name: "Last transaction",
+            value: `${dbinfo.PointHistory[0].comment} ${
+              dbinfo.PointHistory[0].points > 0 ? "+" : ""
+            } ${dbinfo.PointHistory[0].points}:palm_tree:`,
+            inline: false,
+          }
         );
       interaction.editReply({ embeds: [embed], ephemeral: true });
     });
