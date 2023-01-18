f82b5ea22fcf5a9ce31ff836d8bc38d1a1ee66dc
Niels Larmuseau <niels@larmuseau.org>
2023-01-18 16:52:56 +0100

    new command

---
+++ interactions/commands/aiImage.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+//const { EmbedBuilder,ActionRowBuilder, ButtonBuilder } = require('discord.js');
+
+module.exports = {
+  name: "aiImage",
+  data: new SlashCommandBuilder()
+    .setName("aiImage")
+    .setDescription("Generate an image with AI")
+    .addStringOption((option) =>
+      option
+        .setName("message")
+        .setDescription("Image description")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const message = interaction.options.getString("message");
+    const openai = new OpenAIApi(configuration);
+    try {
+      await interaction.deferReply();
+      const response = await openai.createImage({
+        prompt: message,
+        n: 1,
+        size: "1024x1024",
+      });
+      let image_url = response.data.data[0].url;
+      interaction.editReply({ content: message, files: [image_url] });
+    } catch (e) {
+      console.log(e);
+      interaction.editReply({ content: "error" });
+    }
+  },
+  isGuild: true,
+};
