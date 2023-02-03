8f4d4c8bc6938049ba5a12cc9e69c4b621509ddb
Niels Larmuseau <niels@larmuseau.org>
2023-02-03 13:42:44 +0100

    discord api fixes

--- islander/Achievements.ts
+++ islander/Achievements.ts
@@ -1,6 +1,6 @@
 import { createCanvas, Image } from "@napi-rs/canvas";
 import { readFile } from "fs/promises";
-import { AttachmentBuilder } from "discord.js";
+import { AttachmentBuilder, User } from "discord.js";
 const { request } = require("undici");
 
 export class Achievements {
@@ -27,31 +27,24 @@ export class Achievements {
     });
   }
 
-  async GetProfile(memberID: string) {
+  async GetProfile(memberID: string): Promise<AttachmentBuilder> {
     let member = await global.client.prisma.members.findFirst({
       where: {
         ID: memberID,
       },
     });
-
-    //get guild member from id
-    let guildMember = await global.client.guild.members.fetch(memberID);
-
+    let guildMember: User = await global.client.users.fetch(memberID, false);
     const canvas = createCanvas(700, 250);
     const context = canvas.getContext("2d");
-
     const background = await readFile("./wallpaper.jpg");
     const backgroundImage = new Image();
     backgroundImage.src = background;
     context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-
     context.strokeStyle = "#0099ff";
     context.strokeRect(0, 0, canvas.width, canvas.height);
-
     context.font = "28px sans-serif";
     context.fillStyle = "#ffffff";
     context.fillText("Profile", canvas.width / 2.5, canvas.height / 3.5);
-
     context.font = applyText(canvas, `${member.DisplayName}!`);
     context.fillStyle = "#ffffff";
     context.fillText(
@@ -59,19 +52,16 @@ export class Achievements {
       canvas.width / 2.5,
       canvas.height / 1.8
     );
-
     context.beginPath();
     context.arc(125, 125, 100, 0, Math.PI * 2, true);
     context.closePath();
     context.clip();
-
     const { body } = await request(
-      guildMember.avatar.displayAvatarURL({ format: "jpg" })
+      guildMember.displayAvatarURL({ extension: "jpg" })
     );
     const avatar = new Image();
     avatar.src = Buffer.from(await body.arrayBuffer());
     context.drawImage(avatar, 25, 25, 200, 200);
-
     const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), {
       name: "profile-image.png",
     });
