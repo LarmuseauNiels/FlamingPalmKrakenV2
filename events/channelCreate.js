12af2c3941f5fa49388773d08c9f7848562577ec
Niels Larmuseau <niels@larmuseau.org>
2023-03-01 13:34:45 +0100

    fix crash

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,19 +1,5 @@
 module.exports = {
   name: "channelCreate",
   execute(channel) {
-    if (channel instanceof global.client.discord.VoiceChannel) {
-      if (channel.guild.id === "530537522355240961") {
-        global.client.log("new flaming palm voice channel made" + channel.name);
-        global.client.DBconnection.query(
-          "INSERT INTO Channel (ID,ChannelName) VALUES (?,?)",
-          [channel.id, channel.name],
-          function (error, results, fields) {
-            if (error != null) {
-              global.client.log(error);
-            }
-          }
-        );
-      }
-    }
   },
 };
