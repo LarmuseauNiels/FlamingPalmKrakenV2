c14ee1af8fd15f97f1eedf4598d4ebe36b35f6c1
Niels Larmuseau <niels@larmuseau.org>
2023-04-27 14:10:46 +0200

    try ai fix

--- modules/ApiFunctions/MemberEndPoints.ts
+++ modules/ApiFunctions/MemberEndPoints.ts
@@ -74,7 +74,7 @@ export function memberEndPoints(app) {
               RewardItem: {
                 where: {
                   RedeemedBy: {
-                    equals: "",
+                    isEmpty: true,
                   },
                 },
               },
