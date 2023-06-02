1732e7325046e1e35fb19064588823289ca05858
Niels Larmuseau <niels@larmuseau.org>
2023-06-02 16:09:53 +0200

    test

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,16 +2,20 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
   name: "messageDelete",
   execute(msg) {
+    console.log(msg);
+    /*
     const removedMessageEmbed = new EmbedBuilder()
       .setTitle("Message removed")
-      .setDescription(`${msg.author.username}'s message has been removed`)
+      .setDescription(`${msg?.author?.username}'s message has been removed`)
       .addFields(
-        { name: "Message", value: msg.cleanContent, inline: false },
-        { name: "Channel", value: msg.channel.name, inline: false }
+        { name: "Message", value: msg?.cleanContent, inline: false },
+        { name: "Channel", value: msg.channel?.name, inline: false }
       )
       .setColor("#FF0000")
       .setTimestamp();
 
     global.client.logChannel.send({ embeds: [removedMessageEmbed] });
+
+     */
   },
 };
