dcf009f8629bacd261839162eb24f4cc46f02a3b
Niels Larmuseau <niels@larmuseau.org>
2023-07-07 10:49:46 +0200

    fix msg delete

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,9 +4,42 @@ module.exports = {
   execute(msg) {
     console.log(msg);
 
+    const removedMessageEmbed = new EmbedBuilder()
+      .setTitle("Message removed")
+      .setDescription(`${msg?.author?.username}'s message has been removed`)
+      .addFields([
+        {
+          name: "Author ID",
+          value: `${msg?.author?.id}`,
+          inline: true,
+        },
+        {
+          name: "Channel",
+          value: `${msg?.channel?.name}`,
+          inline: true,
+        },
+        {
+          name: "Channel ID",
+          value: `${msg?.channelId}`,
+          inline: true,
+        },
+        {
+          name: "Sent",
+          value: `${msg?.createdAt}`,
+          inline: true,
+        },
+        {
+          name: "Content",
+          value: `${msg?.content}`,
+          inline: false,
+        },
+      ])
+      .setColor("#FF0000")
+      .setTimestamp();
+
     let channel = global.client.channels.cache.get("1126413976155205713");
     if (channel instanceof TextChannel) {
-      channel.send("```json\n" + JSON.stringify(msg) + "```");
+      channel.send({ embeds: [removedMessageEmbed] });
     }
   },
 };
