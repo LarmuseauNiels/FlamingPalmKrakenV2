c21f8ae0b8bc7fe170f8ada65ca59d284056b022
niels <niels@larmuseau.org>
2023-12-18 12:03:36 +0100

    fix

--- interactions/selects/raidsignup.js
+++ interactions/selects/raidsignup.js
@@ -2,8 +2,7 @@ module.exports = {
   name: "raidsignup",
   async execute(interaction) {
     let UserId = interaction.user.id;
-    let SelectedRaid = interaction.values[0];
-
+    let SelectedRaid = parseInt(interaction.values[0]);
 
     await global.client.prisma.raidAttendees.create({
       data: {
