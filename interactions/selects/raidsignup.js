6505c6844bebfe0d0dccd7c8a3568b38ad8bb8b7
niels <niels@larmuseau.org>
2023-12-18 10:26:01 +0100

    raid signup test

--- interactions/selects/raidsignup.js
+++ interactions/selects/raidsignup.js
@@ -1,4 +1,11 @@
 module.exports = {
   name: "raidsignup",
-  async execute(interaction) {},
+  async execute(interaction) {
+    let UserId = interaction.user.id;
+    let SelectedRaid = interaction.value;
+
+    interaction.reply({
+      content: "id: " + UserId + " raid: " + SelectedRaid
+    })
+  },
 };
