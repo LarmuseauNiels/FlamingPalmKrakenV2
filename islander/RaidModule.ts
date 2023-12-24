50fd0e94b1fe6d578807c22b8a7a2633af7b239d
niels <niels@larmuseau.org>
2023-12-24 13:14:31 +0100

    save update

--- islander/RaidModule.ts
+++ islander/RaidModule.ts
@@ -1,6 +1,7 @@
 export class RaidModule {
 
     async AddUserToRaid( userId: string,  raidId: number){
+        //this.SchedulingCreationCheck(raidId);
         return global.client.prisma.raidAttendees.create({
             data: {
                 MemberId: userId,
@@ -8,5 +9,30 @@ export class RaidModule {
             }
         });
     }
+
+    async SchedulingCreationCheck(raidId: number){
+        let raid = await global.client.prisma.raids.findFirst({
+                include: {
+                    RaidAttendees: true
+                },
+                where: {
+                    ID: raidId,
+                    Status: 1
+                }
+            });
+
+        if(raid == null){
+            console.log("Raid not found");
+            return;
+        }
+
+        if(raid.RaidAttendees.length >= raid.MinPlayers){
+            console.log("Raid is full");
+
+
+            return;
+        }
+    }
+
 
 }
