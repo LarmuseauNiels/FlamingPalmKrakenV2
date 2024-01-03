399efe3d751f3861994a646f1624b490c46e26f8
Niels Larmuseau <niels@larmuseau.org>
2024-01-03 09:48:22 +0100

    test

--- islander/RaidModule.ts
+++ islander/RaidModule.ts
@@ -264,7 +264,9 @@ export abstract class RaidModule {
       console.log("Collecting votes for " + attendee.MemberId);
       let user = await global.client.users.fetch(attendee.MemberId);
       console.log("user found: " + user.username);
+      console.log(user.dmChannel);
       let messages = await user.dmChannel.messages.fetch({ limit: 100 });
+      console.log(messages);
       let message = messages.find((m) => m.content == raid.ID.toString());
       for (const option of raid.RaidSchedulingOption) {
         let users = await message.reactions
