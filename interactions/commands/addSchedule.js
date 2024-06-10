d44f1abdf1fc5fbf071fb0d166816c2ba6b254b6
niels <niels@larmuseau.org>
2024-06-10 19:51:03 +0200

    selectable options

--- interactions/commands/addSchedule.js
+++ interactions/commands/addSchedule.js
@@ -21,6 +21,20 @@ module.exports = {
                 .setRequired(true)
                 .setMinValue(1)
                 .setMaxValue(12)
+                .addChoices(
+                    {name : "January", value : 1},
+                    {name : "February", value : 2},
+                    {name : "March", value : 3},
+                    {name : "April", value : 4},
+                    {name : "May", value : 5},
+                    {name : "June", value : 6},
+                    {name : "July", value : 7},
+                    {name : "August", value : 8},
+                    {name : "September", value : 9},
+                    {name : "October", value : 10},
+                    {name : "November", value : 11},
+                    {name : "December", value : 12},
+                )
         )
         .addIntegerOption((option) =>
             option
@@ -29,6 +43,39 @@ module.exports = {
                 .setRequired(true)
                 .setMinValue(1)
                 .setMaxValue(31)
+                .addChoices(
+                    {name : "1", value : 1},
+                    {name : "2", value : 2},
+                    {name : "3", value : 3},
+                    {name : "4", value : 4},
+                    {name : "5", value : 5},
+                    {name : "6", value : 6},
+                    {name : "7", value : 7},
+                    {name : "8", value : 8},
+                    {name : "9", value : 9},
+                    {name : "10", value : 10},
+                    {name : "11", value : 11},
+                    {name : "12", value : 12},
+                    {name : "13", value : 13},
+                    {name : "14", value : 14},
+                    {name : "15", value : 15},
+                    {name : "16", value : 16},
+                    {name : "17", value : 17},
+                    {name : "18", value : 18},
+                    {name : "19", value : 19},
+                    {name : "20", value : 20},
+                    {name : "21", value : 21},
+                    {name : "22", value : 22},
+                    {name : "23", value : 23},
+                    {name : "24", value : 24},
+                    {name : "25", value : 25},
+                    {name : "26", value : 26},
+                    {name : "27", value : 27},
+                    {name : "28", value : 28},
+                    {name : "29", value : 29},
+                    {name : "30", value : 30},
+                    {name : "31", value : 31},
+                )
         )
         .addIntegerOption((option) =>
             option
@@ -37,6 +84,32 @@ module.exports = {
                 .setRequired(true)
                 .setMinValue(0)
                 .setMaxValue(23)
+                .addChoices(
+                    {name : "00", value : 0},
+                    {name : "01", value : 1},
+                    {name : "02", value : 2},
+                    {name : "03", value : 3},
+                    {name : "04", value : 4},
+                    {name : "05", value : 5},
+                    {name : "06", value : 6},
+                    {name : "07", value : 7},
+                    {name : "08", value : 8},
+                    {name : "09", value : 9},
+                    {name : "10", value : 10},
+                    {name : "11", value : 11},
+                    {name : "12", value : 12},
+                    {name : "13", value : 13},
+                    {name : "14", value : 14},
+                    {name : "15", value : 15},
+                    {name : "16", value : 16},
+                    {name : "17", value : 17},
+                    {name : "18", value : 18},
+                    {name : "19", value : 19},
+                    {name : "20", value : 20},
+                    {name : "21", value : 21},
+                    {name : "22", value : 22},
+                    {name : "23", value : 23},
+                )
         )
         .addIntegerOption((option) =>
             option
@@ -45,6 +118,12 @@ module.exports = {
                 .setRequired(true)
                 .setMinValue(0)
                 .setMaxValue(59)
+                .addChoices(
+                    {name : "00", value : 0},
+                    {name : "15", value : 15},
+                    {name : "30", value : 30},
+                    {name : "45", value : 45},
+                )
         )
         .addStringOption((option) =>
                 option
