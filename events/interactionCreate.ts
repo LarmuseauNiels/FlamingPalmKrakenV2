4333ddf35a0a866902ffae3495f15c91bd483fc1
niels <nielslarmuseau@gmail.com>
2025-03-01 09:18:20 +0000

    refactor: streamline interaction handling by creating dedicated methods for each interaction type

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -12,59 +12,75 @@ export default class interactionCreate implements IEvent {
     );
     try {
       if (interaction.isButton()) {
-        console.log(interaction.customId);
-        let buttonId = interaction.customId.split("_")[0];
-        let button = global.client.buttons.get(buttonId);
-        if (button) await button.execute(interaction);
++        await this.handleButton(interaction);
       } else if (interaction.isStringSelectMenu()) {
-        console.log(interaction.customId);
-        let selectID = interaction.customId;
-        let select = global.client.selects.get(selectID);
-        if (select) await select.execute(interaction);
-      } else if (interaction.isMessageContextMenuCommand()) {
-        console.log(interaction.commandName);
-        let menu = global.client.contextMenus.get(interaction.commandName);
-        if (menu) await menu.execute(interaction);
-      } else if (interaction.isUserContextMenuCommand()) {
-        console.log((interaction as any).commandName);
-        let menu = global.client.contextMenus.get(interaction.commandName);
-        if (menu) await menu.execute(interaction);
-      } else {
-        if (interaction.isCommand()) {
-          console.log(interaction.commandName);
-          let command = global.client.commands.get(interaction.commandName);
-          if (command) await command.execute(interaction);
-        }
-      }
-      if (interaction.isModalSubmit()) {
-        let modalId = interaction.customId.split("_")[0];
-        let modal = global.client.modals.get(modalId);
-        if (modal) await modal.execute(interaction);
-      }
-      if (interaction.isAutocomplete()) {
-        let command = global.client.commands.get(interaction.commandName);
-        if (command) await command.autocomplete(interaction);
+        await this.handleSelectMenu(interaction);
+      } else if (interaction.isMessageContextMenuCommand() || interaction.isUserContextMenuCommand()) {
+        await this.handleContextMenu(interaction);
+      } else if (interaction.isCommand()) {
+        await this.handleCommand(interaction);
+      } else if (interaction.isModalSubmit()) {
+        await this.handleModalSubmit(interaction);
+      } else if (interaction.isAutocomplete()) {
+        await this.handleAutocomplete(interaction);
       }
     } catch (error) {
-      global.bugsnag.notify(error);
-      global.client.log(error);
-      if (interaction.isCommand()) {
-        if (interaction.deferred) {
-          await interaction.editReply({
-            content: "A problem occurred. Error has been logged and reported.",
-          });
-        } else {
-          await interaction.reply({
-            content: "A problem occurred. Error has been logged and reported.",
-            ephemeral: true,
-          });
-        }
+      await this.handleError(interaction, error);
+    }
+    global.bugsnag.pauseSession();
+  }
+
+  private async handleButton(interaction: any) {
+    console.log(interaction.customId);
+    let buttonId = interaction.customId.split("_")[0];
+    let button = global.client.buttons.get(buttonId);
+    if (button) await button.execute(interaction);
+  }
+
+  private async handleSelectMenu(interaction: any) {
+    console.log(interaction.customId);
+    let selectID = interaction.customId;
+    let select = global.client.selects.get(selectID);
+    if (select) await select.execute(interaction);
+  }
+
+  private async handleContextMenu(interaction: any) {
+    console.log(interaction.commandName);
+    let menu = global.client.contextMenus.get(interaction.commandName);
+    if (menu) await menu.execute(interaction);
+  }
+
+  private async handleCommand(interaction: any) {
+    console.log(interaction.commandName);
+    let command = global.client.commands.get(interaction.commandName);
+    if (command) await command.execute(interaction);
+  }
+
+  private async handleModalSubmit(interaction: any) {
+    let modalId = interaction.customId.split("_")[0];
+    let modal = global.client.modals.get(modalId);
+    if (modal) await modal.execute(interaction);
+  }
+
+  private async handleAutocomplete(interaction: any) {
+    let command = global.client.commands.get(interaction.commandName);
+    if (command) await command.autocomplete(interaction);
+  }
+
+  private async handleError(interaction: any, error: any) {
+    global.bugsnag.notify(error);
+    global.client.log(error);
+    if (interaction.isCommand()) {
+      if (interaction.deferred) {
+        await interaction.editReply({
+          content: "A problem occurred. Error has been logged and reported.",
+        });
+      } else {
         await interaction.reply({
           content: "A problem occurred. Error has been logged and reported.",
           ephemeral: true,
         });
       }
     }
-    global.bugsnag.pauseSession();
   }
 }
